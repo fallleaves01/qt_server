fbdb047b44e6d174d3cc269170a8a72af5749a3f
fallleaves01 <adoubiq@qq.com>
2022-08-22 12:22:47 +0800

    添加了数据编码的标准格式

---
+++ encoding.cpp
@@ -0,0 +1,39 @@
+#include "encoding.h"
+
+Encoding::Data::Data(int _type,
+                     int _senderUid,
+                     int _receiverUid,
+                     const std::string& _time,
+                     const std::string& _content)
+    : type(_type),
+      senderUid(_senderUid),
+      receiverUid(_receiverUid),
+      time(_time),
+      content(_content) {
+    timeSize = time.length();
+    contentSize = content.length();
+    dataSize = 4 * 6 + timeSize + contentSize;
+}
+
+Encoding::Data::Data(std::string data) {
+    dataSize = decode(data.substr(0, 4));
+    type = decode(data.substr(4, 4));
+    senderUid = decode(data.substr(8, 4));
+    receiverUid = decode(data.substr(12, 4));
+    timeSize = decode(data.substr(16, 4));
+    time = data.substr(20, timeSize);
+    contentSize = decode(data.substr(20 + timeSize, 4));
+    content = data.substr(24 + timeSize);
+}
+
+std::string Encoding::Data::encode() {
+    std::string data = Encoding::encode(dataSize);
+    data += Encoding::encode(type);
+    data += Encoding::encode(senderUid);
+    data += Encoding::encode(receiverUid);
+    data += Encoding::encode(timeSize);
+    data += time;
+    data += Encoding::encode(contentSize);
+    data += content;
+    return data;
+}
--- encoding.h
+++ encoding.h
@@ -1,22 +1,38 @@
 #ifndef ENCODING_H
 #define ENCODING_H
 
-#include<string>
-namespace Encoding {
-    inline std::string encode(int val) {
-        std::string s = "";
-        for (int i = 0; i < 32; i += 8) {
-            s += (unsigned char)((val >> i) & 0xff);
-        }
-        return s;
-    }
-    inline int decode(std::string s) {
-        int val = 0;
-        for (int i = 0; i < 4; i++) {
-            val |= (unsigned char)(s[i]) << (i * 8);
-        }
-        return val;
-    }
-}
+#include <string>
 
-#endif
+namespace Encoding {
+inline std::string encode(int val) {
+    std::string s = "";
+    for (int i = 0; i < 32; i += 8) {
+        s += (unsigned char)((val >> i) & 0xff);
+    }
+    return s;
+}
+inline int decode(std::string s) {
+    int val = 0;
+    for (int i = 0; i < 4; i++) {
+        val |= (unsigned char)(s[i]) << (i * 8);
+    }
+    return val;
+}
+class Data {
+    int dataSize, type, senderUid, receiverUid, timeSize;
+    std::string time;
+    int contentSize;
+    std::string content;
+
+   public:
+    explicit Data(int _type,
+                  int _senderUid,
+                  int _receiverUid,
+                  const std::string& _time,
+                  const std::string& _content);
+    explicit Data(std::string data);
+    std::string encode();
+};
+}  // namespace Encoding
+
+#endif //encoding.h
